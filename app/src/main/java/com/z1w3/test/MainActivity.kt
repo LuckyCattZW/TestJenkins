f8bb47bbfd5d5d29b0a41c142f34a167002dc1ce
CattWei <249414003@qq.com>
2021-01-08 11:02:11 +0800

    first_commit

---
+++ app/src/main/java/com/z1w3/test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.z1w3.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
